1fe1496291d21a287de91f5dadd66aa4c0ec9198
BTR <thilakraj1301@gmail.com>
2025-05-11 17:42:24 -0700

    [S30] second exercise - stack using linkedlist

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,24 +1,35 @@
-
+# Time Complexity:
+#  push(data): O(1)
+#  pop(): O(1)
+# Space Complexity: O(n)
 class Node:
     def __init__(self, data):
-       self.data = data
-       self.next = None
- 
+        self.data = data
+        self.next = None
+
 class Stack:
     def __init__(self):
-        
+        self.top = None
+
     def push(self, data):
-        
+        new_node = Node(data)
+        new_node.next = self.top
+        self.top = new_node
+
     def pop(self):
-        
+        if self.top is None:
+            return None
+        popped_node = self.top
+        self.top = self.top.next
+        return popped_node.data
+
+
 a_stack = Stack()
 while True:
-    #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
     print('push <value>')
     print('pop')
     print('quit')
     do = input('What would you like to do? ').split()
-    #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
     operation = do[0].strip().lower()
     if operation == 'push':
         a_stack.push(int(do[1]))
